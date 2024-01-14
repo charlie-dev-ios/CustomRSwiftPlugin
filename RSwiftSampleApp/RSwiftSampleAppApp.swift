9277f0d630c6b6dc3fdbe031669c974b770ca081
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-14 18:03:32 +0900

    init project

---
+++ RSwiftSampleApp/RSwiftSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSwiftSampleAppApp.swift
+//  RSwiftSampleApp
+//
+//  Created by kotaro-seki on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct RSwiftSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
